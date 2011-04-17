860e7a1da27f212502c5387b0ec7d548ed642fe6
Ilya "FileJunkie" Ershov <filejunkie@gmail.com>
2011-04-17 19:16:08 +0400

    AutobidInfo class implemented

--- src/ru/spbstu/students/dto/AutobidInfo.java
+++ src/ru/spbstu/students/dto/AutobidInfo.java
@@ -1,5 +1,49 @@
 package ru.spbstu.students.dto;
 
+import java.util.Currency;
+
 public class AutobidInfo {
-//TODO: implement class
+	private int user;
+	private int item;
+	private Currency max;
+	private Currency step;
+	
+	public AutobidInfo(int user, int item, Currency max, Currency step) {
+		this.user = user;
+		this.item = item;
+		this.max = max;
+		this.step = step;
+	}
+
+	public int getUser() {
+		return user;
+	}
+
+	public void setUser(int user) {
+		this.user = user;
+	}
+
+	public int getItem() {
+		return item;
+	}
+
+	public void setItem(int item) {
+		this.item = item;
+	}
+
+	public Currency getMax() {
+		return max;
+	}
+
+	public void setMax(Currency max) {
+		this.max = max;
+	}
+
+	public Currency getStep() {
+		return step;
+	}
+
+	public void setStep(Currency step) {
+		this.step = step;
+	}	
 }
