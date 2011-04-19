c0d94878ef21979518b11acf2ae26881317209f9
Ilya "FileJunkie" Ershov <filejunkie@gmail.com>
2011-04-19 22:28:14 +0400

    BidDAO interface

---
+++ src/ru/spbstu/students/dao/BidDAO.java
@@ -0,0 +1,11 @@
+package ru.spbstu.students.dao;
+
+import java.util.List;
+
+import ru.spbstu.students.dto.BidInfo;
+
+public interface BidDAO {
+	public void addBid(BidInfo bid);
+	public void removeBid(int bidID);
+	public List<BidInfo> getBids(int itemID);
+}
